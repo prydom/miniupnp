783a5513d26a777be96ee6383b4f3496ed364d5f
Jordan Pryde <jordan@pryde.me>
2021-09-24 23:32:04 -0700

    Modify the nft_flush.sh script to match the sample configuration

--- miniupnpd/netfilter_nft/scripts/nft_flush.sh
+++ miniupnpd/netfilter_nft/scripts/nft_flush.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-nft flush chain ip nat MINIUPNPD
-nft flush chain ip nat MINIUPNPD-POSTROUTING
-nft flush chain inet filter MINIUPNPD
+nft flush chain ip router MINIUPNPD
+nft flush chain ip router MINIUPNPD-POSTROUTING
+nft flush chain ip6 router MINIUPNPD
+nft flush chain ip6 router MINIUPNPD-POSTROUTING
+nft flush chain inet router MINIUPNPD
